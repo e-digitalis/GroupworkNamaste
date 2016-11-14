35535bf89b7398f6b1a6ac8ddeec9c7a1934be20
AnnSofieF <annsofieforsberg@yahoo.se>
2016-11-14 10:35:27 +0100

    Update EmployeeManagement.java

--- EmployeeManagement.java
+++ EmployeeManagement.java
@@ -2,4 +2,15 @@ package groupworkNamaste;
 
 public class EmployeeManagement {
     
+    //CREATE THE ARRAYLIST FROM THIS CLASS
+    
+    
+    //METHOD FOR ADDING NEW EMPLOYEES
+    
+    
+    //METHOD FOR DELETING EMPLOYEES
+    
+    
+    //CREATE THE NESTED CLASS EmployeeUpdate INSIDE THIS CLASS
+    
 }
